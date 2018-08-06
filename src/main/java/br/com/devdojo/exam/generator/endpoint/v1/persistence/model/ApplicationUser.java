10a514ef012c89b3994b5170950a867fafcd4ca2
WaltecCruz <myusername@myemaildomain.com>
2018-08-06 11:42:09 -0300

    Adicionado Entidades + Ednpoint V1

---
+++ src/main/java/br/com/devdojo/exam/generator/endpoint/v1/persistence/model/ApplicationUser.java
@@ -0,0 +1,46 @@
+package br.com.devdojo.exam.generator.endpoint.v1.persistence.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class ApplicationUser extends AbstractEntity{
+
+    @NotEmpty(message = "The field username cannot be empty")
+    @Column(unique = true)
+    private String userName;
+
+    @NotEmpty(message = "The field password cannot be empty")
+    private String password;
+
+    @OneToOne
+    private Professor professor;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+}
---
+++ src/main/java/br/com/devdojo/exam/generator/endpoint/v1/persistence/model/Professor.java
@@ -0,0 +1,35 @@
+package br.com.devdojo.exam.generator.endpoint.v1.persistence.model;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Professor extends AbstractEntity {
+
+    @NotEmpty (message = "The field name cannot be empty")
+    private String nam;
+
+    @Email(message = "This email is not valid")
+    @NotEmpty( message = "The field email cannot be empty")
+    @Column(unique = true)
+    private String email;
+
+    public String getNam() {
+        return nam;
+    }
+
+    public void setNam(String nam) {
+        this.nam = nam;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
